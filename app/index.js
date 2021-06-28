5825c578e62c03e06ea88f4f54e4d85c2c9d4f15
Ritik Vishwakarma <avikarsh004@gmail.com>
2021-06-28 16:39:06 +0530

    First Commit

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import Popular  from './components/popular'
+import './index.css'
+
+class App extends React.Component {
+    render(){
+        return(
+            <div className='container'>
+                <Popular />
+            </div>
+        )
+    }
+
+}
+
+ReactDom.render(
+    <App />,
+    document.getElementById('app')
+) 
